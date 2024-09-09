8b6d9013751175ba914810f317efacd30b0acef8
unknown <johnpatrickdfelicia.com>
2024-09-09 19:00:37 +0800

    added semantic HTML and OGP meta tags

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg'; // Ensure this path is correct
+
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="Little Lemon Logo" />
+    </header>
+  );
+}
+
+export default Header;
